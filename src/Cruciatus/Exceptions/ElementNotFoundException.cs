3c719c723ff85c68808538f5d7b1b092e74a536a
g.golovin <g.golovin@2Gis.ru>
2013-11-14 18:20:47 +0700

    [UBER-697] Added base ElementNotFound and PropertyNotSupported exceptions.

---
+++ src/Cruciatus/Exceptions/ElementNotFoundException.cs
@@ -0,0 +1,58 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ElementNotFoundException.cs" company="2GIS">
+//   UITestLibrary.Exceptions
+// </copyright>
+// <summary>
+//   Определяет класс исключение ElementNotFoundException.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Cruciatus.Exceptions
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    [Serializable]
+    public class ElementNotFoundException : Exception
+    {
+        public ElementNotFoundException()
+        {
+        }
+
+        public ElementNotFoundException(string element)
+        {
+            this.Element = element;
+        }
+
+        public ElementNotFoundException(string element, string message)
+            : base(message)
+        {
+            this.Element = element;
+        }
+
+        public ElementNotFoundException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected ElementNotFoundException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+
+        public override string Message
+        {
+            get
+            {
+                if (this.Element == null)
+                {
+                    return "Элемент не найден.\n";
+                }
+
+                return string.Format("Элемент {0} не найден.\n", this.Element);
+            }
+        }
+
+        private string Element { get; set; }
+    }
+}
---
+++ src/Cruciatus/Exceptions/PropertyNotSupportedException.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PropertyNotSupportedException.cs" company="2GIS">
+//   UITestLibrary.Exceptions
+// </copyright>
+// <summary>
+//   Определяет класс исключение PropertyNotSupportedException.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Cruciatus.Exceptions
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    [Serializable]
+    public class PropertyNotSupportedException : Exception
+    {
+        public PropertyNotSupportedException()
+        {
+        }
+
+        public PropertyNotSupportedException(string message)
+            : base(message)
+        {
+        }
+
+        public PropertyNotSupportedException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected PropertyNotSupportedException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
