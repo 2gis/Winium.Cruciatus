e938cdb90906cbd4f467890fde081f33c08d8d60
ga.golovin <ga.golovin@gmail.com>
2013-11-15 17:17:30 +0700

    [UBER-705] Added WpfTestApplication.

---
+++ src/TestApplications/WpfTestApplication/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿
+namespace WpfTestApplication
+{
+    using System.Collections.Generic;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region monthsList, timeSizeList
+        private readonly List<string> monthsList = new List<string>
+                                                       {
+                                                           "January",
+                                                           "February",
+                                                           "March",
+                                                           "April",
+                                                           "May",
+                                                           "June",
+                                                           "July",
+                                                           "August",
+                                                           "September",
+                                                           "October",
+                                                           "November",
+                                                           "December"
+                                                       };
+
+        private readonly List<string> timeSizeList = new List<string>
+                                                       {
+                                                           "Day",
+                                                           "Week",
+                                                           "Decade",
+                                                           "Month",
+                                                           "Quarter",
+                                                           "Year"
+                                                       };
+        #endregion
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            this.CheckBox1.IsChecked = true;
+
+            // Fill TextkComboBox
+            foreach (var month in this.monthsList)
+            {
+                this.TextComboBox.Items.Add(new TextBlock { Text = month });
+            }
+
+            // Fill CheckComboBox
+            foreach (var month in this.monthsList)
+            {
+                this.CheckComboBox.Items.Add(new CheckBox { Content = month });
+            }
+
+            // Fill TextkListBox
+            foreach (var month in this.monthsList)
+            {
+                this.TextListBox.Items.Add(new TextBlock { Text = month });
+            }
+
+            // Fill CheckListBox
+            foreach (var month in this.monthsList)
+            {
+                this.CheckListBox.Items.Add(new CheckBox { Content = month });
+            }
+        }
+
+        private void CheckBox1Checked(object sender, RoutedEventArgs e)
+        {
+            this.TextListBox.IsEnabled = true;
+        }
+
+        private void CheckBox1Unchecked(object sender, RoutedEventArgs e)
+        {
+            this.TextListBox.IsEnabled = false;
+        }
+
+        private void CheckBox2Checked(object sender, RoutedEventArgs e)
+        {
+            foreach (CheckBox checkBox in this.CheckListBox.Items)
+            {
+                checkBox.IsChecked = true;
+            }
+        }
+
+        private void CheckBox2Unchecked(object sender, RoutedEventArgs e)
+        {
+            foreach (CheckBox checkBox in this.CheckListBox.Items)
+            {
+                checkBox.IsChecked = false;
+            }
+        }
+
+        private void SetTextButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.TextBox1.Text = "CARAMBA";
+        }
+
+        private void ChangeEnabledButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.TextBox2.IsEnabled = !this.TextBox2.IsEnabled;
+        }
+    }
+}
