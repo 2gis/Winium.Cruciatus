9da9ec7718ad66d7eafd7e8931ac380e1c57c824
g.golovin <g.golovin@2Gis.ru>
2013-11-29 17:01:38 +0700

    [UBER-686] Updated TextBlock class.

--- src/Cruciatus/Elements/TextBlock.cs
+++ src/Cruciatus/Elements/TextBlock.cs
@@ -3,7 +3,7 @@
 //   Cruciatus
 // </copyright>
 // <summary>
-//   Представляет элемент текстовый блок.
+//   Представляет элемент управления текстовый блок.
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
@@ -13,16 +13,65 @@ namespace Cruciatus.Elements
     using System.Windows.Automation;
     using System.Windows.Forms;
 
+    using Cruciatus.Exceptions;
     using Cruciatus.Extensions;
+    using Cruciatus.Interfaces;
 
     using Microsoft.VisualStudio.TestTools.UITesting;
 
     using ControlType = System.Windows.Automation.ControlType;
 
-    public class TextBlock : BaseElement<TextBlock>
+    /// <summary>
+    /// Представляет элемент управления текстовый блок.
+    /// </summary>
+    public class TextBlock : BaseElement<TextBlock>, ILazyInitialize
     {
         private const int MouseMoveSpeed = 2500;
 
+        /// <summary>
+        /// Инициализирует новый экземпляр класса <see cref="TextBlock"/>.
+        /// </summary>
+        public TextBlock()
+        {
+        }
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса <see cref="TextBlock"/>.
+        /// </summary>
+        /// <param name="parent">
+        /// Элемент, являющийся родителем для текстового блока.
+        /// </param>
+        /// <param name="automationId">
+        /// Уникальный идентификатор текстового блока.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// Входные параметры не должны быть нулевыми.
+        /// </exception>
+        public TextBlock(AutomationElement parent, string automationId)
+        {
+            if (parent == null)
+            {
+                throw new ArgumentNullException("parent");
+            }
+
+            if (automationId == null)
+            {
+                throw new ArgumentNullException("automationId");
+            }
+
+            this.Parent = parent;
+            this.AutomationId = automationId;
+        }
+
+        /// <summary>
+        /// Возвращает координаты точки, внутри текстового блока, которые можно использовать для нажатия.
+        /// </summary>
+        /// <exception cref="PropertyNotSupportedException">
+        /// Текстовый блок не поддерживает данное свойство.
+        /// </exception>
+        /// <exception cref="InvalidCastException">
+        /// При получении значения свойства не удалось привести его к ожидаемому типу.
+        /// </exception>
         public System.Drawing.Point ClickablePoint
         {
             get
@@ -33,6 +82,34 @@ namespace Cruciatus.Elements
             }
         }
 
+        /// <summary>
+        /// Возвращает текст из текстового блока.
+        /// </summary>
+        /// <exception cref="PropertyNotSupportedException">
+        /// Текстовый блок не поддерживает данное свойство.
+        /// </exception>
+        /// <exception cref="InvalidCastException">
+        /// При получении значения свойства не удалось привести его к ожидаемому типу.
+        /// </exception>
+        public string Text
+        {
+            get
+            {
+                try
+                {
+                    return this.GetPropertyValue<TextBlock, string>(AutomationElement.NameProperty);
+                }
+                catch (Exception exc)
+                {
+                    this.LastErrorMessage = exc.Message;
+                    return null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Возвращает текстовое представление имени класса.
+        /// </summary>
         internal override string ClassName
         {
             get
@@ -41,7 +118,15 @@ namespace Cruciatus.Elements
             }
         }
 
-        internal override string AutomationId { get; set; }
+        /// <summary>
+        /// Возвращает или задает уникальный идентификатор текстового блока.
+        /// </summary>
+        internal override sealed string AutomationId { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает элемент, который является родителем текстового блока.
+        /// </summary>
+        internal AutomationElement Parent { get; set; }
 
         internal override ControlType GetType
         {
@@ -51,14 +136,31 @@ namespace Cruciatus.Elements
             }
         }
 
+        /// <summary>
+        /// Возвращает инициализированный элемент выпадающего списка.
+        /// </summary>
         internal override AutomationElement Element
         {
             get
             {
+                if (this.element == null)
+                {
+                    this.Find();
+                }
+
                 return this.element;
             }
         }
 
+        /// <summary>
+        /// Выполняет нажатие по текстовому блоку.
+        /// </summary>
+        /// <param name="mouseButton">
+        /// Задает кнопку мыши, которой будет произведено нажатие; либо кнопка по умолчанию.
+        /// </param>
+        /// <returns>
+        /// Значение true если нажать на выпадающий список удалось; в противном случае значение - false.
+        /// </returns>
         public bool Click(MouseButtons mouseButton = MouseButtons.Left)
         {
             try
@@ -76,6 +178,12 @@ namespace Cruciatus.Elements
             return true;
         }
 
+        public void LazyInitialize(AutomationElement parent, string automationId)
+        {
+            this.Parent = parent;
+            this.AutomationId = automationId;
+        }
+
         internal override TextBlock FromAutomationElement(AutomationElement element)
         {
             if (element == null)
@@ -85,6 +193,23 @@ namespace Cruciatus.Elements
 
             this.element = element;
             return this;
+        }
+
+        /// <summary>
+        /// Поиск текстового блока в родительском элементе.
+        /// </summary>
+        private void Find()
+        {
+            var condition = new PropertyCondition(AutomationElement.AutomationIdProperty, this.AutomationId);
+            this.element = CruciatusFactory.WaitingValues(
+                () => this.Parent.FindFirst(TreeScope.Subtree, condition),
+                value => value == null);
+
+            // Если не нашли, то загрузить текстовый блок не удалось
+            if (this.element == null)
+            {
+                throw new ElementNotFoundException(this.ToString());
+            }
         }
     }
 }
