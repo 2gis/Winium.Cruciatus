15bd2baba90c1fa6ce18ab807f27f39da77fa4d0
g.golovin <g.golovin@2Gis.ru>
2013-12-16 12:16:42 +0700

    [UBER-816] Added support for ClickOnce applications.

--- src/Cruciatus/Application.cs
+++ src/Cruciatus/Application.cs
@@ -12,6 +12,8 @@ namespace Cruciatus
     using System;
     using System.Collections.Generic;
     using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
     using System.Threading;
     using System.Windows.Automation;
 
@@ -37,6 +39,18 @@ namespace Cruciatus
 
         private AutomationElement mainWindowElement;
 
+        /// <summary>
+        /// Инициализирует новый экземпляр приложения.
+        /// </summary>
+        /// <param name="fullPath">
+        /// Полный путь к исполняемому файлу *.exe (к *.appref-ms в случае ClickOnce приложения).
+        /// </param>
+        /// <param name="mainWindowAutomationId">
+        /// Уникальный идентификатор главного окна приложения.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// Входные параметры не должны быть нулевыми.
+        /// </exception>
         protected Application(string fullPath, string mainWindowAutomationId)
         {
             if (fullPath == null)
@@ -72,11 +86,32 @@ namespace Cruciatus
             }
         }
 
+        /// <summary>
+        /// Запуск приложения с временем ожидания по умолчанию.
+        /// </summary>
+        /// <returns>
+        /// Значение true если запуск приложения удался; в противном случае значение - false.
+        /// </returns>
+        /// <exception cref="Exception">
+        /// Произвести процесс запуска приложения не удалось.
+        /// </exception>
         public bool Start()
         {
             return this.Start(CruciatusFactory.Settings.SearchTimeout);
         }
 
+        /// <summary>
+        /// Запуск приложения с временем ожидания по умолчанию.
+        /// </summary>
+        /// <param name="milliseconds">
+        /// Задает время ожидания запуска приложения.
+        /// </param>
+        /// <returns>
+        /// Значение true если запуск приложения удался; в противном случае значение - false.
+        /// </returns>
+        /// <exception cref="Exception">
+        /// Произвести процесс запуска приложения не удалось.
+        /// </exception>
         public bool Start(int milliseconds)
         {
             this.process = Process.Start(this.fullPath);
@@ -87,6 +122,82 @@ namespace Cruciatus
                 throw new Exception("Не удалось запустить процесс");
             }
 
+            this.mainWindowElement = CruciatusFactory.WaitingValues(
+                    () => WindowFactory.GetMainWindowElement(this.process.Id, this.mainWindowAutomationId),
+                    value => value == null,
+                    milliseconds);
+            return this.mainWindowElement != null;
+        }
+
+        /// <summary>
+        /// Запуск ClickOnce приложения с временем ожидания по умолчанию (необходимо указать имя процесса приложения).
+        /// </summary>
+        /// <param name="processName">
+        /// Имя процесса.
+        /// </param>
+        /// <returns>
+        /// Значение true если запуск ClickOnce приложения удался; в противном случае значение - false.
+        /// </returns>
+        /// <exception cref="Exception">
+        /// Произвести процесс запуска ClickOnce приложения не удалось.
+        /// </exception>
+        public bool StartClickOnce(string processName)
+        {
+            return this.StartClickOnce(processName, CruciatusFactory.Settings.SearchTimeout);
+        }
+
+        /// <summary>
+        /// Запуск ClickOnce приложения с заданным временем ожидания (необходимо указать имя процесса приложения).
+        /// </summary>
+        /// <param name="processName">
+        /// Имя процесса.
+        /// </param>
+        /// <param name="milliseconds">
+        /// Задает время ожидания запуска ClickOnce приложения.
+        /// </param>
+        /// <returns>
+        /// Значение true если запуск ClickOnce приложения удался; в противном случае значение - false.
+        /// </returns>
+        /// <exception cref="Exception">
+        /// Произвести процесс запуска ClickOnce приложения не удалось.
+        /// </exception>
+        public bool StartClickOnce(string processName, int milliseconds)
+        {
+            // Условие для искомого процесса
+            var func = new Func<Process, bool>(p => p.ProcessName == processName);
+            
+            // Получаем существующие процессы с полученным именем и убиваем их
+            var processes = Process.GetProcesses().ToList().Where(func);
+            foreach (var proc in processes)
+            {
+                proc.Kill();
+                if (!proc.WaitForExit(milliseconds))
+                {
+                    throw new Exception("Не удалось убить старые экземпляры приложений.");
+                }
+            }
+
+            // Запуск ClickOnce приложения
+            var clickOnceApp = Process.Start(this.fullPath);
+            if (clickOnceApp == null)
+            {
+                throw new Exception("Не удалось запустить ClickOnce приложение.");
+            }
+
+            // Ожидание завершения ClickOnce приложения
+            clickOnceApp.WaitForExit();
+
+            // Ожидаем появление подходящего процесса
+            this.process = CruciatusFactory.WaitingValues(
+                               () => Process.GetProcesses().ToList().FirstOrDefault(func),
+                               value => value == null,
+                               milliseconds);
+            if (this.process == null)
+            {
+                throw new Exception("Не удалось запустить приложение.");
+            }
+
+            // Ожидаем открытие главного окна
             this.mainWindowElement = CruciatusFactory.WaitingValues(
                     () => WindowFactory.GetMainWindowElement(this.process.Id, this.mainWindowAutomationId),
                     value => value == null,
