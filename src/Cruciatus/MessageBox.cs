dcd2e730b96b51cc400c523573fe4b7f6b8f2a1e
g.golovin <g.golovin@2Gis.ru>
2014-01-09 13:52:40 +0700

    UBER-839] Added static class MessageBox and extension for Window (CloseMessageBox).

---
+++ src/Cruciatus/MessageBox.cs
@@ -0,0 +1,106 @@
+﻿
+namespace Cruciatus
+{
+    using System.Windows;
+    using System.Windows.Automation;
+
+    using Cruciatus.Elements;
+
+    using Window = Cruciatus.Elements.Window;
+
+    internal static class MessageBox
+    {
+        internal static int NumberOfOpenModalWindow(Window window)
+        {
+            var condition = new PropertyCondition(WindowPattern.IsModalProperty, true);
+            return window.Element.FindAll(TreeScope.Children, condition).Count;
+        }
+
+        internal static bool ClickButton(Window window, MessageBoxButton buttonsType, MessageBoxResult button)
+        {
+            var condition = new PropertyCondition(WindowPattern.IsModalProperty, true);
+            var modalwindow = window.Element.FindFirst(TreeScope.Children, condition);
+            if (modalwindow == null)
+            {
+                return false;
+            }
+
+            string uid;
+            if (button == MessageBoxResult.None)
+            {
+                uid = "Close";
+            }
+            else
+            {
+                switch (buttonsType)
+                {
+                    case MessageBoxButton.OK:
+                        switch (button)
+                        {
+                            case MessageBoxResult.OK:
+                                uid = "2";
+                                break;
+                            default:
+                                return false;
+                        }
+
+                        break;
+
+                    case MessageBoxButton.OKCancel:
+                        switch (button)
+                        {
+                            case MessageBoxResult.OK:
+                                uid = "1";
+                                break;
+                            case MessageBoxResult.Cancel:
+                                uid = "2";
+                                break;
+                            default:
+                                return false;
+                        }
+
+                        break;
+
+                    case MessageBoxButton.YesNo:
+                        switch (button)
+                        {
+                            case MessageBoxResult.Yes:
+                                uid = "6";
+                                break;
+                            case MessageBoxResult.No:
+                                uid = "7";
+                                break;
+                            default:
+                                return false;
+                        }
+
+                        break;
+
+                    case MessageBoxButton.YesNoCancel:
+                        switch (button)
+                        {
+                            case MessageBoxResult.Yes:
+                                uid = "6";
+                                break;
+                            case MessageBoxResult.No:
+                                uid = "7";
+                                break;
+                            case MessageBoxResult.Cancel:
+                                uid = "2";
+                                break;
+                            default:
+                                return false;
+                        }
+
+                        break;
+
+                    default:
+                        return false;
+                }
+            }
+
+            var buttonElement = new Button(modalwindow, uid);
+            return buttonElement.Click();
+        }
+    }
+}
