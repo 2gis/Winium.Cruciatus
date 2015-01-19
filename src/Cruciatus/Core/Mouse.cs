1d26a6130baa6928cab8aa86df013e394b5d9f24
Golovin Gleb <g.golovin@2gis.ru>
2015-01-19 12:11:34 +0600

    Add VerticalScroll method to Button class

--- src/Cruciatus/Core/Mouse.cs
+++ src/Cruciatus/Core/Mouse.cs
@@ -9,6 +9,8 @@
 
     public static class Mouse
     {
+        private const int MouseWheelClickSize = 120;
+
         public const int MouseEventLeftDown = 0x0002;
 
         public const int MouseEventLeftUp = 0x0004;
@@ -16,6 +18,8 @@
         public const int MouseEventRightDown = 0x0008;
 
         public const int MouseEventRightUp = 0x0010;
+
+        public const int MouseEventWheel = 0x0800;
 
         public static bool SetCursorPos(double x, double y)
         {
@@ -78,6 +82,22 @@
         {
             RightClick();
             RightClick();
+        }
+
+        public static void VerticalScroll(int amountOfClicks)
+        {
+            int data;
+            try
+            {
+                data = checked(MouseWheelClickSize * amountOfClicks);
+            }
+            catch (OverflowException oe)
+            {
+                throw new ArgumentOutOfRangeException("amountOfClicks", oe.ToString());
+            }
+
+            MouseNativeMethods.mouse_event(MouseEventWheel, 0, 0, data, IntPtr.Zero);
+            Thread.Sleep(50);
         }
     }
 }
--- src/Cruciatus/Core/MouseNativeMethods.cs
+++ src/Cruciatus/Core/MouseNativeMethods.cs
@@ -14,6 +14,6 @@ namespace Cruciatus.Core
         internal static extern bool SetCursorPos(int x, int y);
 
         [DllImport("user32.dll")]
-        internal static extern void mouse_event(uint dwFlags, int dx, int dy, uint cButtons, IntPtr dwExtraInfo);
+        internal static extern void mouse_event(uint dwFlags, int dx, int dy, int dwData, IntPtr dwExtraInfo);
     }
 }
