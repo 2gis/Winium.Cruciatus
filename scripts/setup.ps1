e21d58f1758fce8197a5c7a5e23d7c9808b5bdf5
Golovin Gleb <g.golovin@2gis.ru>
2015-08-26 11:15:34 +0600

    Add ci script (for build and run tests)

---
+++ scripts/setup.ps1
@@ -0,0 +1,31 @@
+﻿Param(
+    [string[]]$modules
+)
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+#------------------------------
+
+$configuration = 'Release'
+$solution = Join-Path $PSScriptRoot '..\src\Winium.sln'
+$testFiles = ,"..\src\TestApplications.Tests\WindowsFormsTestApplication.Tests\bin\$configuration\WindowsFormsTestApplication.Tests.dll"
+$testFiles += "..\src\TestApplications.Tests\WpfTestApplication.Tests\bin\$configuration\WpfTestApplication.Tests.dll"
+
+$msbuildProperties = @{
+    'Configuration' = $configuration
+    'Defineconstants' = 'REMOTE'
+}
+
+$env:UITestApps=(Join-Path $PSScriptRoot 'UITestApps')
+
+$modulesUrl = 'https://raw.githubusercontent.com/skyline-gleb/dev-help/v0.2.0/psm'
+
+if (!(Get-Module -ListAvailable -Name PsGet))
+{
+    (new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex
+}
+
+foreach ($module in $modules)
+{
+    Install-Module -ModuleUrl "$modulesUrl/$module.psm1" -Update
+}
